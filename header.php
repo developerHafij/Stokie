d4a4ca1d95805a725a9f147f2116d93d5b871e9d
Hafijur Rahman <developer.hafij@gmail.com>
2020-05-16 15:00:10 +0600

    upload stokie demo

---
+++ header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+
+    <!-- Custom CSS  -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Responsive -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+    <title>Stokie || E-commerce site</title>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- Include Header File -->
+<?php include "header.php" ?>
+
+<!-- Include Navbar -->
+<?php include "navbar.php" ?>
+
+
+<h1>Hello, world!</h1>
+
+
+<!-- Include Footer File -->
+<?php include "footer.php" ?>
+
+    
